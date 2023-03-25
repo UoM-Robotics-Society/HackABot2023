7f12fe0748a915d0f392441f29d9da7200f57473
Hensage <59978499+Hensage@users.noreply.github.com>
2023-03-25 17:52:53 +0000

    Add files via upload

--- nano.ino
+++ nano.ino
@@ -39,7 +39,6 @@ void setup() {
   while (!Serial) {
     // some boards need to wait to ensure access to serial over USB
   }
-
   // initialize the transceiver on the SPI bus
   if (!radio.begin()) {
     Serial.println(F("radio hardware is not responding!!"));
@@ -89,64 +88,23 @@ void setup() {
 }  // setup
 
 void loop() {
-
-  if (role) {
-    // This device is a TX node
-
-    unsigned long start_timer = micros();                // start the timer
-    Serial.print(payload);
-    bool report = radio.write(&payload, sizeof(int));  // transmit & save the report
-    unsigned long end_timer = micros();                  // end the timer
-    if (!report){
-      Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
-    }else{
-      Serial.println(F("All good"));  // payload was not delivered
-    }
-    /*
-    if (report) {
-      Serial.print(F("Transmission successful! "));  // payload was delivered
-      Serial.print(F("Time to transmit = "));
-      Serial.print(end_timer - start_timer);  // print the timer result
-      Serial.print(F(" us. Sent: "));
-      Serial.println(payload);  // print payload sent
-      payload += 0.01;          // increment float payload
-    } else {
-      Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
-    }
-    */
-    // to make this example readable in the serial monitor
-    delay(1000);  // slow transmissions down by 1 second
-
-  }/* else {
-    // This device is a RX node
-
-    uint8_t pipe;
-    if (radio.available(&pipe)) {              // is there a payload? get the pipe number that recieved it
-      uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
-      radio.read(&payload, bytes);             // fetch payload from FIFO
-      Serial.print(F("Received "));
-      Serial.print(bytes);  // print the size of the payload
-      Serial.print(F(" bytes on pipe "));
-      Serial.print(pipe);  // print the pipe number
-      Serial.print(F(": "));
-      Serial.println(payload);  // print the payload's value
-    }
-  }  // role
-  */
   if (Serial.available()) {
     // change the role via the serial monitor
 
     char c = toupper(Serial.read());
     if (c == 'W') {
-      // Become the TX node
-      payload = 2;
-
+      payload = 1;
     } else if (c == 'S') {
       payload = 2;
     }
-    else{
-      payload = 2;
-    }
+    //Serial.print(payload);
   }
+  bool report = radio.write(&payload, sizeof(int));  // transmit & save the report
+  if (report){
+    Serial.print("All good in the ");
+  }else{
+    Serial.print("All bad in the hood");
+  }
+  delay(1000);  // slow transmissions down by 1 second
 
 }  // loop
