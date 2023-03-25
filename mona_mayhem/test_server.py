40b800a0a8f9abb8c29936c351584747471c3485
Thomas Leitch <thomas.leitch@student.manchester.ac.uk>
2023-03-25 15:19:55 +0000

    scraper and server

---
+++ mona_mayhem/test_server.py
@@ -0,0 +1,50 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("""
+        <html>
+            <head>
+            <title>
+            MONA and Ball locations
+            </title>
+            </head>
+            <body>
+            <h1>MONA and Ball Locations.
+            </h1><br>C1,1580,966,2.96
+
+            <br>G43,231,535,1.59
+
+            <br>G42,1641,519,-0.05
+
+            <br>M24,507,518,0.4
+
+            <br>M2,1293,372,-0.42
+
+            <br>C0,294,65,-1.5
+
+            <br>B,911,585,0
+
+            </body>
+        </html>
+        """, "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
--- mona_mayhem/webscraper.py
+++ mona_mayhem/webscraper.py
@@ -1,5 +1,57 @@
 
+#webscraping
 import requests
-response = requests.get(f"http://192.168.4.1")
+
+#command line args
+import sys
+
+#visulaization
+from tkinter import *
+
+#window
+window=Tk()
+window.title('Fusbol')
+
+local_server = "http://localhost:8080"
+main_server = "http://192.168.4.1"
+
+if sys.argv[1] == "main":
+    response = requests.get(main_server)
+else:
+    response = requests.get(local_server)
 html_content = response.text
 print(html_content)
+data = [x.strip().split(',') for x in html_content.split("<br>")[1:-1]]
+locations = {}
+for x in data:
+    locations[x[0]] = [float(i) for i in x[1:]]
+
+print(locations)
+
+window.geometry("1500x1000")
+
+#Create a canvas object
+c = Canvas(window,width=1500,height=1000)
+
+boundry = 200
+x_off = locations['C0'][0] - boundry
+y_off = locations['C0'][1] - boundry
+scale = 0.5
+
+for k in locations:
+    locations[k] = [(locations[k][0]-x_off)*scale, (locations[k][1]-y_off)*scale, x[2]]
+
+c.create_rectangle(locations["C0"][0],locations["C0"][1],locations["C1"][0],locations["C1"][1],outline = "black",fill = "green",width = 1)
+
+print(locations)
+
+r = 20
+#Draw an Oval in the canvas
+for i in range(len(locations.values())):
+    l = list(locations.values())[i]
+    k = list(locations.keys())[i]
+    c.create_oval(l[0]-r, l[1]-r, l[0]+r, l[1]+r, outline = "black",fill = "black",width = 2)
+    c.create_text(l[0], l[1], text=k, fill="white", font=('Helvetica 15 bold'))
+    c.pack()
+
+window.mainloop()
