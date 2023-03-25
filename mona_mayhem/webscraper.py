f7242c70eca4ad66ef07c75caee7e0504de3ff51
Thomas Leitch <thomas.leitch@student.manchester.ac.uk>
2023-03-25 21:48:11 +0000

    commit

--- mona_mayhem/webscraper.py
+++ mona_mayhem/webscraper.py
@@ -2,6 +2,7 @@
 #webscraping
 from cgi import test
 import math
+import time
 import requests
 import re
 
@@ -36,27 +37,6 @@ starting = True
 while (starting):
     try:
         # setup 
-        window=Tk()
-        window.title('Fusbol')
-
-        window.geometry("1500x1000")
-
-        #Create a canvas object
-        c = Canvas(window,width=1500,height=1000)
-
-        locations = get_loc(server)
-
-        boundry = 200
-        x_off = locations['C0'][0] - boundry
-        y_off = locations['C0'][1] - boundry
-        scale = 0.5
-
-        for k in locations:
-            locations[k] = [(locations[k][0]-x_off)*scale, (locations[k][1]-y_off)*scale, locations[k][2]]
-
-        c.create_rectangle(locations["C0"][0],locations["C0"][1],locations["C1"][0],locations["C1"][1],outline = "black",fill = "green",width = 1)
-        c.pack()
-
         starting = False
     
     except:
@@ -64,16 +44,15 @@ while (starting):
 
 while True:
 
-    #locations = get_loc(server)
+    locations = get_loc(server)
 
     try:
         ball = locations['B']
-        p8 = locations['M8']
-        p7 = locations['M7']
+        p8 = locations['M2']
+        #p7 = locations['M7']
 
         p8_ang = math.atan((ball[1]-p8[1])/(ball[0]-p8[0]))
         p8_diff = p8[2] - p8_ang
-        c.create_line(p8[0], p8[1], p8[0]+math.cos(p8_ang)*500, p8[1]+math.sin(p8_ang)*500, fill='yellow')
         print(p8_diff)
         if (p8_diff >= 0):
             print("turn left")
@@ -83,7 +62,30 @@ while True:
     except:
         print("ball error")
 
+    time.sleep(1)
+
 #NOTE tkinter code
+
+window=Tk()
+window.title('Fusbol')
+
+window.geometry("1500x1000")
+
+#Create a canvas object
+c = Canvas(window,width=1500,height=1000)
+
+locations = get_loc(server)
+
+boundry = 200
+x_off = locations['C0'][0] - boundry
+y_off = locations['C0'][1] - boundry
+scale = 0.5
+
+for k in locations:
+    locations[k] = [(locations[k][0]-x_off)*scale, (locations[k][1]-y_off)*scale, locations[k][2]]
+
+c.create_rectangle(locations["C0"][0],locations["C0"][1],locations["C1"][0],locations["C1"][1],outline = "black",fill = "green",width = 1)
+c.pack()
 
 r = 20
 for i in range(len(locations.values())):
