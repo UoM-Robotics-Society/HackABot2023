25f80fb1bf7f4c495e80f16fe2d3cec6f3be8f7b
Thomas Leitch <thomas.leitch@student.manchester.ac.uk>
2023-03-25 18:02:57 +0000

    more

--- mona_mayhem/webscraper.py
+++ mona_mayhem/webscraper.py
@@ -1,5 +1,6 @@
 
 #webscraping
+import math
 import requests
 
 #command line args
@@ -15,13 +16,16 @@ window.title('Fusbol')
 local_server = "http://localhost:8080"
 main_server = "http://192.168.4.1"
 
+
 if sys.argv[1] == "main":
     response = requests.get(main_server)
+    html_content = response.text
+    data = [x.strip().split(',') for x in html_content.split("<br>")[2:-1]]
 else:
     response = requests.get(local_server)
-html_content = response.text
-print(html_content)
-data = [x.strip().split(',') for x in html_content.split("<br>")[1:-1]]
+    html_content = response.text
+    data = [x.strip().split(',') for x in html_content.split("<br>")[1:-1]]
+
 locations = {}
 for x in data:
     locations[x[0]] = [float(i) for i in x[1:]]
@@ -39,7 +43,7 @@ y_off = locations['C0'][1] - boundry
 scale = 0.5
 
 for k in locations:
-    locations[k] = [(locations[k][0]-x_off)*scale, (locations[k][1]-y_off)*scale, x[2]]
+    locations[k] = [(locations[k][0]-x_off)*scale, (locations[k][1]-y_off)*scale, locations[k][2]]
 
 c.create_rectangle(locations["C0"][0],locations["C0"][1],locations["C1"][0],locations["C1"][1],outline = "black",fill = "green",width = 1)
 
@@ -52,6 +56,8 @@ for i in range(len(locations.values())):
     k = list(locations.keys())[i]
     c.create_oval(l[0]-r, l[1]-r, l[0]+r, l[1]+r, outline = "black",fill = "black",width = 2)
     c.create_text(l[0], l[1], text=k, fill="white", font=('Helvetica 15 bold'))
+    print(l[0], l[1], l[0]+math.cos(l[2]), l[1]+math.sin(l[2]))
+    c.create_line(l[0], l[1], l[0]+math.cos(l[2])*50, l[1]+math.sin(l[2])*50, fill='red')
     c.pack()
 
 window.mainloop()
