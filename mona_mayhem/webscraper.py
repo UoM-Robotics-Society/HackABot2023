0f2798f4c877e7c69f76a2a7ed5279c8073c79d1
Nabiel Augustino <nabiel.augustino@student.manchester.ac.uk>
2023-03-25 18:37:27 +0000

    Looping through get requests

--- mona_mayhem/webscraper.py
+++ mona_mayhem/webscraper.py
@@ -16,38 +16,38 @@ window.title('Fusbol')
 local_server = "http://localhost:8080"
 main_server = "http://192.168.4.1"
 
+while True:
+    if sys.argv[1] == "main":
+        response = requests.get(main_server)
+        html_content = response.text
+        data = [x.strip().split(',') for x in html_content.split("<br>")[2:-1]]
+    else:
+        response = requests.get(local_server)
+        html_content = response.text
+        data = [x.strip().split(',') for x in html_content.split("<br>")[1:-1]]
 
-if sys.argv[1] == "main":
-    response = requests.get(main_server)
-    html_content = response.text
-    data = [x.strip().split(',') for x in html_content.split("<br>")[2:-1]]
-else:
-    response = requests.get(local_server)
-    html_content = response.text
-    data = [x.strip().split(',') for x in html_content.split("<br>")[1:-1]]
+    locations = {}
+    for x in data:
+        locations[x[0]] = [float(i) for i in x[1:]]
 
-locations = {}
-for x in data:
-    locations[x[0]] = [float(i) for i in x[1:]]
+    print(locations)
 
-print(locations)
+    # window.geometry("1500x1000")
 
-window.geometry("1500x1000")
+    # #Create a canvas object
+    # c = Canvas(window,width=1500,height=1000)
 
-#Create a canvas object
-c = Canvas(window,width=1500,height=1000)
+    # boundry = 200
+    # x_off = locations['C0'][0] - boundry
+    # y_off = locations['C0'][1] - boundry
+    # scale = 0.5
 
-boundry = 200
-x_off = locations['C0'][0] - boundry
-y_off = locations['C0'][1] - boundry
-scale = 0.5
+    # for k in locations:
+    #     locations[k] = [(locations[k][0]-x_off)*scale, (locations[k][1]-y_off)*scale, locations[k][2]]
 
-for k in locations:
-    locations[k] = [(locations[k][0]-x_off)*scale, (locations[k][1]-y_off)*scale, locations[k][2]]
+    # c.create_rectangle(locations["C0"][0],locations["C0"][1],locations["C1"][0],locations["C1"][1],outline = "black",fill = "green",width = 1)
 
-c.create_rectangle(locations["C0"][0],locations["C0"][1],locations["C1"][0],locations["C1"][1],outline = "black",fill = "green",width = 1)
-
-print(locations)
+    # print(locations)
 
 r = 20
 #Draw an Oval in the canvas
